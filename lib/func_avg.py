9936692d17c405b872c2080fc7ec3581a79e31c8
Guangy_li <guangy.li@hotmail.com>
2020-03-09 03:19:52 -0400

    implement the basic average function and visual verification in main file

--- lib/func_avg.py
+++ lib/func_avg.py
@@ -1,7 +1,9 @@
 # Python program that takes numpy array as input signal
 # and reduce the effect of noise using average function
 
+import statistics
 import numpy as np
+import random
 
 class average_signal:
     # Initialize the class
@@ -10,8 +12,48 @@ class average_signal:
         self.input_freq = input_freq
         self.noise_freq = noise_freq
         
+    def generate_average_data(self, steps):
+        
+        # Error handling
+        if not isinstance(steps, int):
+            raise TypeError("Sorry. 'steps' must be an integer.")
+        if not steps >= 0:
+            raise ValueError("Sorry. 'steps' must be zero or positive.")
+        
+        # Function that calculate the average of # steps
+        def calculate_step_average(data_in, steps):
+            temp_data = np.copy(data_in)
+            for i in range(int(len(temp_data)/steps)):
+                
+                last = (i+1)*steps
+                if last >= len(temp_data):
+                    cur_step = temp_data[i*steps:]
+                else:
+                    cur_step = temp_data[i*steps:last]
+                
+                avg_cur_step = np.mean(cur_step, dtype=np.float32)
+                
+                for s in range(steps):
+                    cur_pos = i*steps + s
+                    if cur_pos < len(temp_data):
+                        temp_data[cur_pos] = avg_cur_step
+                
+            return temp_data
+        
+        temp_result = calculate_step_average(self.data, steps)
+        return temp_result
 
 if __name__ == "__main__":
     # Functional level verification starts here
     print("--------average_signal class functional verification--------\n")
+    test_in = []
+    for i in range(100):
+        test_in.append(random.randint(0,200))
+    test_in = np.array(test_in)
+    test = average_signal(test_in, 334, 167)
+    test_result = test.generate_average_data(5)
+    print("test input is:")
+    print(test_in)
+    print("test result is:")
+    print(test_result)
     print("--------Verification ends--------\n")
--- noise_reduction_main.py
+++ noise_reduction_main.py
@@ -1,19 +1,34 @@
+import time
 import numpy as np
+import matplotlib.pyplot as plt
+import noisereduce as nr
 from lib.file_load import FileIn
 from lib.func_avg import average_signal
 
 if __name__ == "__main__":
     # Functional verification starts here
     print("--------Main file functional verification--------\n")
-    test = FileIn("lib/data/google1.txt", 334)
+    test = FileIn("lib/data/google2.txt", 334)
     print("File located in %s" %test.get_location())
     print("File header is %s" %test.get_header())
     print(test.get_data())
     print("Overall average is %.2f" %test.get_average())
     print("The signal frequency is %d\n" %test.get_frequency())
     
-    test_avg = average_signal(test.data, test.freq, 167)
+    temp_data = test.data
+    
+    test_avg = average_signal(temp_data, test.freq, 167)
     print(test_avg.data)
     print(test_avg.input_freq)
     print(test_avg.noise_freq)
+    reduced_result = test_avg.generate_average_data(5)
+    
+    #noisy_part = np.array(list(temp_data[860:])*5)
+    #reduced_result = nr.reduce_noise(audio_clip=temp_data, noise_clip=noisy_part, verbose=False)
+    fig, ax = plt.subplots(figsize=(40,6))
+    ax.plot(temp_data, c='b', label='initial')
+    ax.plot(reduced_result, c='r', label='reduced')
+    #ax.plot(noisy_part, c='g', label='noise')
+    plt.legend(loc='best');
+    plt.show()
     print("--------Verification ends--------\n")
