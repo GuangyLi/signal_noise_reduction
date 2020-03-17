ea051673284a3d4f3a425785f5ac6877542360b6
Guangy_li <guangy.li@hotmail.com>
2020-03-17 00:28:27 -0400

    add function that finds the index of edge

--- lib/data_adjust.py
+++ lib/data_adjust.py
@@ -13,15 +13,30 @@ import matplotlib.pyplot as plt
 # Rising and Falling edge, control with current
 
 # Funtion that aligns file in files close to same start point
-def auto_align(files, edge = "rising", value="auto"):
+def auto_align(files, edge = "rising", tvalue="auto"):
     # Error handling
     if not isinstance(files, list):
         raise TypeError("Sorry. 'files' must be list.")
     if not isinstance(files[0], FileIn):
-        raise TypeError("Sorry. items in 'files' must be FileIn type.")    
+        raise TypeError("Sorry. items in 'files' must be FileIn type.")
     
+    # Rising edge triggered or falling edge triggered, default to rising
+    if (edge != "rising") and (edge != "falling"):
+        raise TypeError("Sorry. 'edge' must be either rising or falling.")
     
+    # Triggered at which value, default to automatic generation
+    if tvalue != "auto":
+        if not isinstance(tvalue, int):
+            raise TypeError("Sorry. 'tvalue' must be an integer.")
+        if not tvalue >= 0:
+            print("WARNING. 'tvalue' should be positive for most cases.")
     
+    for f in files:
+        cur_fe = find_first_edge(f, "rising", int(f.average)+1, 2)
+        print("avg is %d" %f.average)
+        print("%s first edge is %d" %(f.file_name, cur_fe))
+    
+    # Adjust the files after alignment
     auto_adjust(files)
     
     return
@@ -35,6 +50,48 @@ def auto_adjust(files):
         raise TypeError("Sorry. items in 'files' must be FileIn type.")    
     
     return
+
+# Function that find the first rising/falling edge
+def find_first_edge(file, edge, tvalue, min_pw):
+    # Error handling
+    if not isinstance(file, FileIn):
+        raise TypeError("Sorry. 'file' must be FileIn type.")
+    
+    # Rising edge triggered or falling edge triggered, default to rising
+    if (edge != "rising") and (edge != "falling"):
+        raise TypeError("Sorry. 'edge' must be either rising or falling.")
+    
+    # Triggered at which value, default to automatic generation
+    if tvalue != "auto":
+        if not isinstance(tvalue, int):
+            raise TypeError("Sorry. 'tvalue' must be an integer.")
+        if not tvalue >= 0:
+            print("WARNING. 'tvalue' should be positive for most cases.")
+            
+    temp_data = file.data
+    data_len = len(temp_data)
+    pivot = min_pw
+    first_edge = -1
+    
+    while (pivot < data_len-min_pw):
+        start_point = pivot-min_pw
+        end_point = pivot+min_pw
+        is_edge = 1
+        for i in range(min_pw):
+            if edge == "rising":
+                if (temp_data[start_point+i] > tvalue*0.8) or (temp_data[end_point-i] < tvalue*1.2):
+                    is_edge = 0
+            else:
+                if (temp_data[start_point+i] < tvalue*0.8) or (temp_data[end_point-i] > tvalue*1.2):
+                    is_edge = 0
+        
+        if is_edge:
+            first_edge = pivot
+            break
+        
+        pivot += 1
+        
+    return first_edge
 
 if __name__ == "__main__":
     # Functional level verification starts here
